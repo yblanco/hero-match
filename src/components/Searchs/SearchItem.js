d45d645aa8753353b362bc85cd918a0254a15f4d
Yeinso Blanco <yeinsojbm@gmail.com>
2020-01-03 13:58:04 -0500

    adding img to searcher

--- src/components/Searchs/SearchItem.js
+++ src/components/Searchs/SearchItem.js
@@ -8,7 +8,10 @@ export default ({ items= [], onClick=()=>{} }) => (
       items.length > 0
       ? items.map(item =>(
         <div className="searcher-suggest-item" onClick={() => onClick(item.id)} key={item.id}>
-          {item.name}
+          <figure>
+            <img src={item.image.url} alt={item.name} />
+          </figure>
+          <div>{item.name}</div>
         </div>
       ))
       : "Character with given name not found"
